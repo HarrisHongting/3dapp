9fcc426c786794ef49545b26597194459e24d40c
HarrisHongting <78964771+HarrisHongting@users.noreply.github.com>
2023-05-11 21:06:29 +0800

    Add files via upload

--- app/views/header.php
+++ app/views/header.php
@@ -2,6 +2,7 @@
 
 <head>
   <title><?= $pageTitle ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
   <!-- Font Awesome -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
